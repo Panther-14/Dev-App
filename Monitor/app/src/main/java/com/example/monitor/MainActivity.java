dc99500861a2c8bae4d6472d19f4a2906c2aff30
Panther-14 <lego.ninjago.city@gmail.com>
2023-04-22 15:48:39 -0600

    Practica 8

--- Monitor/app/src/main/java/com/example/monitor/MainActivity.java
+++ Monitor/app/src/main/java/com/example/monitor/MainActivity.java
@@ -1,9 +1,11 @@
 package com.example.monitor;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.example.monitor.databinding.ActivityMainBinding;
 
@@ -16,16 +18,17 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        binding = ActivityMainBinding.inflate(getLayoutInflater ());
-
-        setContentView (binding.getRoot());
-
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        MainViewModel viewModel = new ViewModelProvider(this).get(MainViewModel.class);
         binding.eqRecycler.setLayoutManager(new LinearLayoutManager(this));
-
         EqAdapter adapter = new EqAdapter();
+        adapter.setOnItemClickListener(earthquake -> Toast.makeText(MainActivity.this, earthquake.getPlace(), Toast.LENGTH_SHORT).show());
         binding.eqRecycler.setAdapter(adapter);
-        List<Earthquake> eqList = null;
-        adapter.submitList(eqList);
+        viewModel.getEqList().observe(this, eqList -> {
+            adapter.submitList(eqList);
+        });
+        viewModel.getEarthquakes();
+
     }
 }
